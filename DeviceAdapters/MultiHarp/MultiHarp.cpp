e34cd008c11ca3d800d0da33d663e61d76bfe138
Sunil <sunil.kumar@imperial.ac.uk>
2023-03-13 13:54:43 +0000

    MH adapter initial commit

---
+++ DeviceAdapters/MultiHarp/MultiHarp.cpp
@@ -0,0 +1,119 @@
+//#ifndef _WIN32
+//#include <unistd.h>
+//#define Sleep(msec) usleep(msec*1000)
+//#define __int64 long long
+//#else
+//#include <windows.h>
+//#include <dos.h>
+//#include <conio.h>
+//#endif
+
+#include "../../MMDevice/ModuleInterface.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include "MultiHarp.h"
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//// Exported MMDevice API
+/////////////////////////////////////////////////////////////////////////////////
+//MODULE_API void InitializeModuleData()
+//{
+//	RegisterDevice(g_MHDeviceName, MM::GenericDevice, "PicoQuant TCSPC box: MultiHarp");
+//}
+//
+//MODULE_API MM::Device* CreateDevice(const char* deviceName)
+//{
+//	if (deviceName == 0)
+//		return 0;
+//
+//	if (strcmp(deviceName, g_MHDeviceName) == 0)
+//	{
+//		return new MultiHarp();
+//	}
+//
+//	return 0;
+//}
+//
+//MODULE_API void DeleteDevice(MM::Device* pDevice)
+//{
+//	delete pDevice;
+//}
+//
+
+/////////////////////////////////////////////////////////
+//For the piezo pump
+/////////////////////////////////////////////////////////
+
+
+MultiHarp::MultiHarp() : CGenericBase<MultiHarp>()
+{
+	// From some uM base functions included?
+//	InitializeDefaultErrorMessages();
+}
+
+
+MultiHarp::~MultiHarp()
+{
+}
+//
+//void MultiHarp::GetName(char* name) const
+//{
+//	// Return the name used to refer to this device adapter
+//	CDeviceUtils::CopyLimitedString(name, g_MHDeviceName);
+//}
+//
+//int MultiHarp::Initialize()
+//{
+//	if (initialized_)
+//		return DEVICE_OK;
+//
+//	// Name
+//	int ret = CreateProperty(MM::g_Keyword_Name, g_MHDeviceName, MM::String, true);
+//	if (DEVICE_OK != ret)
+//		return ret;
+//
+//	// Description
+//	ret = CreateProperty(MM::g_Keyword_Description, "Bartels piezo pump - QuadKey", MM::String, true);
+//	if (DEVICE_OK != ret)
+//		return ret;
+//
+//	//Adding options exposed to the MM GUI here:
+//	ret = CreateStringProperty(g_PropName_VER, "VERSION", false, new CPropertyAction(this, &MultiHarp::Get_Ver));
+//	if (DEVICE_OK != ret)
+//		return ret;
+//
+//	//changedTime_ = GetCurrentMMTime();
+//	initialized_ = true;
+//	return DEVICE_OK;
+//}
+//
+//int MultiHarp::Shutdown()
+//{
+//	if (initialized_)
+//	{
+//		initialized_ = false;
+//	}
+//	return DEVICE_OK;
+//}
+//
+//bool MultiHarp::Busy()
+//{
+//	/*if (GetCurrentMMTime() - changedTime_ < command_wait_time) {
+//		return true;
+//	}
+//	else {*/
+//		return false;
+//	//}
+//}
+//
+//
+//int MultiHarp::Get_Ver(MM::PropertyBase* pProp, MM::ActionType eAct)
+//{
+//	return DEVICE_OK;
+//}
---
+++ DeviceAdapters/MultiHarp/WORKING COMPILE MultiHarp - Copy.h
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef _WIN32
+#define _stdcall
+#endif
+
+#include "resource.h"
+#include "mhdefin.h"
+#include "errorcodes.h"
+#include "mhlib.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "../../MMDevice/MMDevice.h"
+#include "../../MMDevice/DeviceBase.h"
+#include "../../MMDevice/DeviceUtils.h"
+#include "../../MMDevice/ModuleInterface.h"
+
+static const char* g_MHDeviceName = "PicoQuant MultiHarp";
+static const char* g_PropName_VER = "DLL VERSION";
+
+/////////////////////////////////////////////
+// Predefined constants
+//////////////////////////////////////////
+#define command_wait_time				100
+
+class MultiHarp : public CGenericBase<MultiHarp>
+{
+public:
+	MultiHarp();
+	~MultiHarp();
+
+	// MMDevice API
+	// ------------
+	// Functions here are found in the .cpp file
+	int Initialize();
+	int Shutdown();
+	void GetName(char* pszName) const;
+	bool Busy();
+
+	//Action interface
+	//----------------
+	int Get_Ver(MM::PropertyBase* pProp, MM::ActionType eAct);
+
+private:
+	bool initialized_;
+
+	//MM::MMTime changedTime_;
+};
+
